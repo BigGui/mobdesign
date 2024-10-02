08cada57ae49c313cf3facf5c1a1f70bb0dd7b48
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-10-02 11:01:58 +0200

    load json product datas

--- js/home.js
+++ js/home.js
@@ -1,3 +1,30 @@
 import '/scss/style.scss';
 
 import './components/header.js';
+
+/**
+ * Turn a string into a timestamp.
+ * @param {string} date Given date, string format
+ * @returns {number} Timestamp
+ */
+function getTimestamp(date) {
+    date = new Date(date);
+    return date.getTime();
+}
+
+/**
+ * Load datas from Json and display the last products.
+ */
+async function displayLastProducts() {
+    try {
+        const response = await fetch('/data/products.json');
+        const products = await response.json();
+        products.sort((a, b) => getTimestamp(b.dateAdded) - getTimestamp(a.dateAdded));
+        console.table(products.slice(0, 4));
+    }
+    catch (error) {
+        console.error('Unable to load products data ' + error);
+    }
+}
+
+displayLastProducts();
