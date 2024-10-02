272b3fd8529d74565f6bc4be27772b7f560ecafb
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-10-02 16:37:39 +0200

    menu mobile

--- js/components/header.js
+++ js/components/header.js
@@ -6,3 +6,9 @@ document.addEventListener('scroll', () => {
         document.querySelector('.js-header').classList.add('header--scrolled');
     }
 });
+
+const header = document.getElementById('header');
+header.querySelector('.js-burger')
+    .addEventListener('click', () => {
+        header.classList.toggle('header--opened');
+    });
